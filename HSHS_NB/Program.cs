6162b4bc76540e5229fe54f949f90c4eca1635df
Jerrod Rathsack <JerrodR@americollect.com>
2020-02-18 09:22:17 -0600

    Add project files.

---
+++ HSHS_NB/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSHS_NB
+{
+    class Program
+    {
+        [STAThread]     // make sure this is in your program - very necessary!
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
